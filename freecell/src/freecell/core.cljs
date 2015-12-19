3464e87d176fecc47b15a61a751f2d8caadfcf90
virvar <revalka@gmail.com>
2015-12-20 01:16:49 +0300

    Drag cards pile

--- freecell/src/freecell/core.cljs
+++ freecell/src/freecell/core.cljs
@@ -1,6 +1,8 @@
 (ns freecell.core
   (:require [reagent.core :as r]
-            [goog.string :refer [unescapeEntities]]))
+            [goog.string :refer [unescapeEntities]]
+            [goog.events :as events])
+  (:import [goog.events EventType]))
 
 (def state (r/atom 0))
 (def card-width 80)
@@ -31,24 +33,43 @@
       (js/setTimeout #(swap! seconds inc) 1000)
       [:p.elapsed-time "Elapsed: " @seconds])))
 
+(defn get-client-location
+  [node]
+  (let [rect (.getBoundingClientRect node)]
+    {:x (.-left rect)
+     :y (.-top rect)}))
+
 ;; TODO need block id
 (defn select-pile
   [position event]
-  (let [offset {:x (.-clientX event)
-                :y (.-clientY event)}]
+  (let [origin-location (get-client-location (.getElementById js/document "board"))
+        target-location (get-client-location (.-target event))
+        offset {:x (- (.-clientX event) (:x target-location))
+                :y (- (.-clientY event) (:y target-location))}
+        location {:x (- (.-clientX event) (:x origin-location) (:x offset))
+                  :y (- (.-clientY event) (:y origin-location) (:y offset))}]
     (swap! state #(assoc % :draggable-pile {:id position
-                                            :offset offset}))))
+                                            :offset offset
+                                            :location location}))))
+
+(defn move-pile
+  [event]
+  (let [origin-location (get-client-location (.getElementById js/document "board"))
+        offset (get-in @state [:draggable-pile :offset])
+        location {:x (- (.-clientX event) (:x origin-location) (:x offset))
+                  :y (- (.-clientY event) (:y origin-location) (:y offset))}]
+    (swap! state #(assoc-in % [:draggable-pile :location] location))))
 
 (defn deselect-pile
   [event]
+  (events/unlisten js/window EventType.MOUSEMOVE move-pile)
   (swap! state #(assoc % :draggable-pile nil)))
 
-;; (defn move-pile
-;;   [event]
-;;   (let [offset {:x (.-clientX event)
-;;                 :y (.-clientY event)}]
-;;     (swap! state #(update % :draggable-pile {:id position
-;;                                              :offset offset}))))
+(defn on-card-click
+  [position event]
+  (select-pile position event)
+  (events/listen js/window EventType.MOUSEMOVE move-pile)
+  (events/listen js/window EventType.MOUSEUP deselect-pile))
 
 (defn card-component
   [card position]
@@ -61,10 +82,10 @@
                 :diamonds "red"
                 :clubs "black"
                 :spades "black")]
-    [:div.card-place.card
+    [:div.unselectable.card-place.card
      {:style {:top (str (* position mini-card-height) "px") :color color}
       :on-mouse-down (fn [event]
-                       (select-pile position event))
+                       (on-card-click position event))
       :on-mouse-up (fn [event]
                      (deselect-pile event))}
      (str rank-html suit-html)]))
@@ -80,12 +101,22 @@
          ^{:key (:key card)} [card-component card position])
        [:div.card-place placeholder])]))
 
+(defn pile-component-at
+  [cards location placeholder]
+  (let [height (+ card-height (* (count cards) mini-card-height))]
+    [:div.cards-pile {:style {:left (str (:x location) "px")
+                              :top (str (:y location) "px")}}
+     (if (not (empty? cards))
+       (for [position (range 0 (count cards))
+             :let [card (nth cards position)]]
+         ^{:key (:key card)} [card-component card position])
+       [:div.card-place placeholder])]))
+
 (defn draggable-pile-component
   []
   (when-let [pile (:draggable-pile @state)]
-    (let [position 12.5
-          placeholder "D"]
-      [pile-component () position placeholder])))
+    (let [placeholder "D"]
+      [pile-component-at () (:location pile) placeholder])))
 
 (defn cards-block-component
   ([piles]
@@ -111,7 +142,7 @@
 
 (defn board-component
   []
-  [:div.board
+  [:div#board.board
    ^{:key :freecells} [:div.row [freecells-component] [foundations-component]]
    ^{:key :tableau} [:div.row [tableau-component]]
    ^{:key :draggable-pile} [draggable-pile-component]
