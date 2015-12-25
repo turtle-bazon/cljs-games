dd0029f940584611096f218606dfdd63100fdf5d
Azamat S. Kalimoulline <turtle@bazon.ru>
2015-12-25 15:32:19 +0300

    Initial memory game - pictures and basic react rendering.

--- memory-game/project.clj
+++ memory-game/project.clj
@@ -1,9 +1,11 @@
 (defproject memory-game "0.1.0-SNAPSHOT"
   :description "FIXME: write this!"
-  :url "http://exampl.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/clojurescript "1.7.145"]]
-  :plugins [[lein-cljsbuild "1.1.0"]]
+                 [org.clojure/clojurescript "1.7.170"]
+                 [reagent "0.5.1"]]
+  :plugins [[lein-cljsbuild "1.1.2"]]
   :cljsbuild {:builds
               [{:source-paths ["src"]
                 :compiler {:output-to "resources/public/js/main.js"
--- memory-game/src/memory-game/core.cljs
+++ memory-game/src/memory-game/core.cljs
@@ -1,7 +1,50 @@
-(ns memroy-game.core)
+(ns memroy-game.core
+  (:require
+   [reagent.core :as r]))
 
-(defn start []
-  )
+;;; easy - 4x5
+;;; normal - 6x7
+;;; hard - 8x9
 
-(set! (.-onload js/window) start)
+(def game (r/atom {}))
+
+(defn card-component [card]
+  [:div.card {:type (if (= (:state card) :closed)
+                      "closed"
+                      (:type-id card))}])
+
+(defn board-component []
+  (let [cur-game @game]
+    [:div.board
+     (for [row (-> cur-game :board)]
+       [:div.board-row
+        (for [card row]
+          [card-component card])])]))
+
+(defn game-component []
+  (let [cur-game @game]
+    [:div
+     [board-component]]))
+
+(defn new-game [game' level]
+  (let [[w h] (case level
+                :easy [4 5]
+                :normal [6 7]
+                :hard [8 9])
+        total-cards (* w h)
+        ids-to-play (range 0 (/ total-cards 2))
+        board (vec (map vec (partition
+                             w
+                             (for [id (shuffle (concat ids-to-play ids-to-play))]
+                               {:type-id id
+                                :state :closed}))))]
+    (-> game'
+        (assoc-in [:board] board))))
+
+(defn start [level]
+  (swap! game new-game level)
+  (r/render-component [game-component]
+                      (.-body js/document)))
+
+(set! (.-onload js/window) #(start :normal))
 
