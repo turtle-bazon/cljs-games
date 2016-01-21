8bd95470a446373c4faa9e5afa86ca8be3515fd9
Azamat S. Kalimoulline <turtle@bazon.ru>
2016-01-21 23:44:35 +0300

    Correct names

--- divided-attention/project.clj
+++ divided-attention/project.clj
@@ -3,7 +3,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/clojurescript "1.7.170"]
+                 [org.clojure/clojurescript "1.7.228"]
                  [phzr "0.1.0-SNAPSHOT"]]
   :plugins [[lein-cljsbuild "1.1.2"]]
   :source-paths ["src"]
