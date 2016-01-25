a44c0ab07925b9add48173d5897e3b3546848a3d
Azamat S. Kalimoulline <turtle@bazon.ru>
2016-01-25 23:58:08 +0300

    Stop turtle move and resume by tap

--- turtle-attention/src/turtle_attention/characters.cljs
+++ turtle-attention/src/turtle_attention/characters.cljs
@@ -4,11 +4,14 @@
    [phzr.animation-manager :as animation-manager]
    [phzr.core :as pcore]
    [phzr.game-object-factory :as object-factory]
-   [phzr.group :as group]))
+   [phzr.group :as group]
+   [phzr.signal :as signal]))
 
-(defn add-turtle [game turtles-group x y]
+(defn add-turtle [game turtles-group x y tap-listener]
   (let [turtle (group/create turtles-group x y "turtle")]
     (animation-manager/add (:animations turtle) "right" [0 1 2 3] 8 true)
     (animation-manager/add (:animations turtle) "left" [4 5 6 7] 8 true)
     (utils/set-attr! turtle [:body :collide-world-bounds] true)
+    (utils/set-attr! turtle [:input-enabled] true)
+    (signal/add (get-in turtle [:events :on-input-down]) tap-listener turtle)
     turtle))
--- turtle-attention/src/turtle_attention/play_state.cljs
+++ turtle-attention/src/turtle_attention/play_state.cljs
@@ -10,12 +10,21 @@
 (defonce *play-atom* (atom {}))
 
 (defn turtle-left [turtle]
+  (set! (.-direction turtle) :left)
   (utils/set-attr! turtle [:body :velocity :x] -25)
   (animation-manager/play (:animations turtle) "left"))
 
 (defn turtle-right [turtle]
+  (set! (.-direction turtle) :right)
   (utils/set-attr! turtle [:body :velocity :x] 25)
   (animation-manager/play (:animations turtle) "right"))
+
+(defn turtle-stop [turtle]
+  (utils/set-attr! turtle [:body :velocity :x] 0)
+  (animation-manager/stop (:animations turtle))
+  (utils/set-attr! turtle [:frame] (case (.-direction turtle)
+                                     :right 0
+                                     :left 4)))
 
 (defn turtle-box-collide [turtle box]
   (let [touch-state (:touching (:body turtle))]
@@ -23,24 +32,33 @@
       (.-left touch-state) (turtle-right turtle)
       (.-right touch-state) (turtle-left turtle))))
 
+(defn turtle-tapped [turtle]
+  (if (= (get-in turtle [:body :velocity :x]) 0)
+    (case (.-direction turtle)
+      :right (turtle-right turtle)
+      :left (turtle-left turtle))
+    (turtle-stop turtle)))
+
 (defn state-create [game]
   (let [turtles-group (object-factory/physics-group (:add game))
         boxes-group (object-factory/physics-group (:add game))
         box1 (group/create boxes-group 0 0 "box")
         box2 (group/create boxes-group 736 0 "box")
-        turtle (characters/add-turtle game turtles-group 64 0)]
+        turtle (characters/add-turtle game turtles-group 64 0 (fn [turtle] (turtle-tapped turtle)))]
     (utils/set-attr! box1 [:body :immovable] true)
     (utils/set-attr! box2 [:body :immovable] true)
+    (set! (.-direction turtle) :right)
     (turtle-right turtle)
     (swap! *play-atom*
            (fn [play-atom]
              (-> play-atom
-                 (assoc-in [:turtles] turtles-group)
-                 (assoc-in [:boxes] boxes-group))))))
+                 (assoc-in [:turtles-group] turtles-group)
+                 (assoc-in [:boxes-group] boxes-group))))))
 
 (defn state-update [game]
-  (let [{:keys [turtles boxes]} @*play-atom*]
-    (arcade-physics/collide (:arcade (:physics game)) turtles boxes turtle-box-collide)))
+  (let [{:keys [turtles-group boxes-group]} @*play-atom*]
+    (arcade-physics/collide (:arcade (:physics game))
+                            turtles-group boxes-group turtle-box-collide)))
 
 (def state-obj
   {:create state-create
