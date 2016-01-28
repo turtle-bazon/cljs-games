8c0ad2b9e78f722c8ee691f2506f6b683de245dc
Reval Akmalov <revalka@gmail.com>
2016-01-28 23:41:10 +0300

    Minimal game play

--- bubbles/src/bubbles/play_state.cljs
+++ bubbles/src/bubbles/play_state.cljs
@@ -4,51 +4,65 @@
    [phzr.game-object-factory :as object-factory]
    [phzr.group :as group]
    [phzr.physics.arcade :as arcade-physics]
-   [bubbles.characters :as characters]
+   [bubbles.bubble :as bubble]
    [bubbles.utils :as utils]))
 
 (defonce state-atom (atom {}))
 
 (defonce score-atom (atom 0))
+(defonce lives-atom (atom 10))
+(defonce bubble-create-interval (atom 1000))
 
 (defn add-score [game]
   (object-factory/text (:add game) 64 64 (str "Score: " @score-atom)
-                       {:font "65px Arial",
+                       {:font "32px Arial",
                         :fill "#ff0044",
                         :align "center"}))
 
 (defn show-score [score-text]
   (.setText score-text (str "Score: " @score-atom)))
 
-(defn bubble-right [bubble]
-  (set! (.-direction bubble) :up)
-  (utils/set-attr! bubble [:body :velocity :y] 5))
+(defn add-lives [game]
+  (object-factory/text (:add game) 256 64 (str "Lives: " @lives-atom)
+                       {:font "32px Arial",
+                        :fill "#ff0044",
+                        :align "center"}))
 
-(defn bubble-stop [bubble]
-  (utils/set-attr! bubble [:body :velocity :y] 0))
+(defn show-lives [lives-text]
+  (.setText lives-text (str "Lives: " @lives-atom)))
 
-(defn bubble-tapped [bubble]
-  (bubble-stop bubble)
-  (swap! score-atom inc))
+(defn add-bubble [game group]
+  (when (< 0 @lives-atom)
+    (let [bubble (bubble/add-random-bubble game group
+                                           (fn [bubble]
+                                             (swap! score-atom inc)))]
+      (bubble/bubble-up bubble)
+      (.add (.. game -time -events)
+            @bubble-create-interval
+            (fn [] (add-bubble game group)) nil)
+      (swap! bubble-create-interval (fn [t] (* t 0.95))))))
 
 (defn state-create [game]
   (let [bubbles-group (object-factory/physics-group (:add game))
-        bubble (characters/add-bubble game bubbles-group 64 400
-                                      (fn [bubble] (bubble-tapped bubble)))
-        score-text (add-score game)]
-    (set! (.-direction bubble) :up)
+        score-text (add-score game)
+        lives-text (add-lives game)]
+    (add-bubble game bubbles-group)
     (swap! state-atom
            (fn [state]
              (-> state
                  (assoc-in [:bubbles-group] bubbles-group)
-                 (assoc-in [:score-text] score-text))))))
+                 (assoc-in [:score-text] score-text)
+                 (assoc-in [:lives-text] lives-text))))))
 
 (defn state-update [game]
-  (let [{:keys [bubbles-group score-text]} @state-atom]
+  (let [{:keys [bubbles-group score-text lives-text]} @state-atom]
+    (doall (map (fn [bubble]
+                  (bubble/bubble-update bubble
+                                        (fn []
+                                          (swap! lives-atom dec))))
+                (.-children bubbles-group)))
     (show-score score-text)
-    ;; (arcade-physics/collide (:arcade (:physics game))
-    ;;                         turtles-group boxes-group turtle-box-collide)
-    ))
+    (show-lives lives-text)))
 
 (def state-obj
   {:create state-create
