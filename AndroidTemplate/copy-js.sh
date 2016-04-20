f38e95ce91eb914c812c6f98f70890e1bffb986f
Azamat S. Kalimoulline <turtle@bazon.ru>
2016-04-20 18:38:43 +0300

    Remove AndroidTemplate, many things should be specific

--- AndroidTemplate/copy-js.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "copy"
-# cp -fr ../bubbles/resources/public/js/ app/src/main/assets/www/
-cp -f ../bubbles/resources/public/js/main.js app/src/main/assets/www/js/main.js
-echo "done"
